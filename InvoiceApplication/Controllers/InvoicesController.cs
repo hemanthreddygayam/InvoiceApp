1de42bcf332c4347de810c3c8d26c26c04cbe098
Sai Praveen Gampa <saipraveen.gampa@gmail.com>
2017-11-16 23:58:58 -0600

    Add files

--- InvoiceApplication/Controllers/InvoicesController.cs
+++ InvoiceApplication/Controllers/InvoicesController.cs
@@ -6,16 +6,23 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using InvoiceApplication.DbModels;
 using Microsoft.AspNetCore.Authorization;
+using InvoiceApplication.Models;
+using Microsoft.AspNetCore.Http;
+using System.Text.RegularExpressions;
+
 
 namespace InvoiceApplication.Controllers
 {
     public class InvoicesController : Controller
     {
+        
         private TrackingDbContext _context;
+        private string _userAgent;
 
-        public InvoicesController(TrackingDbContext context)
+        public InvoicesController(TrackingDbContext context, IHttpContextAccessor httpContext)
         {
             _context = context;
+            _userAgent = httpContext.HttpContext.Request.Headers["User-Agent"].ToString();
         }
 
         [Authorize]
@@ -23,7 +30,21 @@ namespace InvoiceApplication.Controllers
         {
             ViewBag.Name = User.FindFirst(ClaimTypes.NameIdentifier).Value;
             IEnumerable<VesselDocuments> documents = _context.VesselDocuments.Where(doc => doc.InvoiceId == id);
+            ViewBag.IsMobile = IsMobileDevice();
             return View(documents);
+        }
+
+        [HttpPost]
+        public JsonResult GetFilePath([FromBody]int id)
+        {
+            return Json(_context.VesselDocuments.Find(id));
+        }
+
+        private Boolean IsMobileDevice()
+        {
+            Regex regex = new Regex(@"(iPhone|Android|iPad|Mobile|iPod|webOS|BlackBerry|Windows Phone)");
+            Match match = regex.Match(_userAgent);
+            return match.Success;
         }
     }
 }
